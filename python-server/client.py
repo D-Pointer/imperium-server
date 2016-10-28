1d26df33dcbc9f19819e879ba717530ce2aabb0c
Jan Ekholm <jan.ekholm@d-pointer.com>
2016-10-28 22:30:08 +0300

    Python server work

--- python-server/client.py
+++ python-server/client.py
@@ -1,5 +1,6 @@
 from twisted.protocols.basic import Int16StringReceiver
-
+import struct
+from tcp_packet import TcpPacket
 
 class Client(Int16StringReceiver):
 
@@ -10,14 +11,24 @@ class Client(Int16StringReceiver):
         self.id = Client.nextId
         Client.nextId += 1
 
+        # set up the handlers
+        self.handlers = {
+            TcpPacket.LOGIN: self.handleLogin,
+            TcpPacket.ANNOUNCE: self.handleLogin,
+            TcpPacket.LEAVE_GAME: self.handleLogin,
+            TcpPacket.JOIN_GAME: self.handleLogin,
+            TcpPacket.DATA: self.handleLogin,
+            TcpPacket.READY_TO_START: self.handleLogin,
+            TcpPacket.GET_RESOURCE_PACKET: self.handleGetResource,
+            TcpPacket.KEEPALIVE_PACKET: self.handleKeepAlivePacket,
+        }
 
     def connectionMade(self):
         self.clients[ self.id ] = self
         print "connectionMade: clients now: %d" % len(self.clients)
 
         # self.factory was set by the factory's default buildProtocol:
-        self.transport.write( 'Hello world!\r\n')
-        #self.transport.loseConnection()
+        #self.transport.write( 'Hello world!\r\n')
 
 
     def connectionLost(self, reason):
@@ -28,4 +39,48 @@ class Client(Int16StringReceiver):
 
 
     def stringReceived(self, string):
-        print "stringReceived: received %d bytes" % len(string)
+        # get the first byte, the packet type
+        (packetType, ) = struct.unpack_from( '!H', string, 0 )
+        print "stringReceived: packet type: %d, name: %s" % (packetType, TcpPacket.name(packetType))
+
+        # find a handler to handle the real packet
+        if not self.handlers.has_key( packetType ):
+            print "invalid packet type: %d" % packetType
+            self.transport.loseConnection()
+            return
+
+        # call the handler
+        self.handlers[ packetType ]( string )
+
+
+    def handleLogin(self, data):
+        offset = 0
+        (packetType, protocol, nameLength) = struct.unpack_from( '!HHH', data, offset )
+        offset += struct.calcsize('!HHH')
+        print "handleLogin: protocol: %d" % protocol
+
+        # name
+        (name, passwordLength) = struct.unpack_from( '!%dsH' % nameLength, data, offset )
+        offset += struct.calcsize( '%dsH' % nameLength )
+        print "handleLogin: name: '%s'" % name
+
+        # password
+        (password) = struct.unpack_from( '!%ds' % passwordLength, data, offset )
+        print "handleLogin: password: '%s'" % password
+
+
+    def handleGetResource(self, data):
+        offset = 0
+        (packetType, resourceNameLength) = struct.unpack_from( '!HH', data, offset )
+        offset += struct.calcsize('!HH')
+
+        # resource name
+        (resourceName, ) = struct.unpack_from( '%ds' % resourceNameLength, data, offset )
+        print "handleGetResource: name: '%s'" % resourceName
+
+
+    def handleKeepAlivePacket(self, data):
+        offset = 0
+        (packetType, ) = struct.unpack_from( '!H', data, offset )
+        offset += struct.calcsize('!H')
+        print "handleKeepAlivePacket"
---
+++ python-server/tcp_packet.py
@@ -0,0 +1,70 @@
+
+class TcpPacket:
+    # TCP packets
+    LOGIN = 0  # to server
+    LOGIN_OK = 1  # from server
+    INVALID_PROTOCOL = 2  # from
+    ALREADY_LOGGED_IN = 3  # from
+    INVALID_NAME = 4  # from
+    NAME_TAKEN = 5  # from
+    SERVER_FULL = 6  # from
+    ANNOUNCE = 7  # to
+    ANNOUNCE_OK = 8 # from
+    ALREADY_ANNOUNCED = 9 # from
+    GAME_ADDED = 10 # from
+    GAME_REMOVED = 11 # from
+    LEAVE_GAME = 12 # to
+    NO_GAME = 13 # from
+    JOIN_GAME = 14 # to
+    GAME_JOINED = 15 # from
+    INVALID_GAME = 16 # from
+    ALREADY_HAS_GAME = 17 # from
+    GAME_FULL = 18 # from
+    GAME_ENDED = 19 # from
+    DATA = 20 # to
+    READY_TO_START = 21 # to
+    GET_RESOURCE_PACKET = 22 # to
+    RESOURCE_PACKET = 23 # from
+    INVALID_RESOURCE_NAME_PACKET = 24 # from
+    INVALID_RESOURCE_PACKET = 25 # from
+    KEEPALIVE_PACKET = 26 # to
+    PLAYER_COUNT_PACKET = 27 # from
+
+    packetNames = {
+        LOGIN: 'LOGIN',
+        LOGIN_OK: 'LOGIN_OK',
+        INVALID_PROTOCOL: 'INVALID_PROTOCOL',
+        ALREADY_LOGGED_IN: 'ALREADY_LOGGED_IN',
+        INVALID_NAME: 'INVALID_NAME',
+        NAME_TAKEN: 'NAME_TAKEN',
+        SERVER_FULL: 'SERVER_FULL',
+        ANNOUNCE: 'ANNOUNCE',
+        ANNOUNCE_OK: 'ANNOUNCE_OK',
+        ALREADY_ANNOUNCED: 'ALREADY_ANNOUNCED',
+        GAME_ADDED: 'GAME_ADDED',
+        GAME_REMOVED: 'GAME_REMOVED',
+        LEAVE_GAME: 'LEAVE_GAME',
+        NO_GAME: 'NO_GAME',
+        JOIN_GAME: 'JOIN_GAME',
+        GAME_JOINED: 'GAME_JOINED',
+        INVALID_GAME: 'INVALID_GAME',
+        ALREADY_HAS_GAME: 'ALREADY_HAS_GAME',
+        GAME_FULL: 'GAME_FULL',
+        GAME_ENDED: 'GAME_ENDED',
+        DATA: 'DATA',
+        READY_TO_START: 'READY_TO_START',
+        GET_RESOURCE_PACKET: 'GET_RESOURCE_PACKET',
+        RESOURCE_PACKET: 'RESOURCE_PACKET',
+        INVALID_RESOURCE_NAME_PACKET: 'INVALID_RESOURCE_NAME_PACKET',
+        INVALID_RESOURCE_PACKET: 'INVALID_RESOURCE_PACKET',
+        KEEPALIVE_PACKET: 'KEEPALIVE_PACKET',
+        PLAYER_COUNT_PACKET: 'PLAYER_COUNT_PACKET'
+    }
+
+
+    @staticmethod
+    def name(packetType):
+        if not TcpPacket.packetNames.has_key(packetType):
+            return '<UNKNOWN>'
+
+        return TcpPacket.packetNames[packetType]
